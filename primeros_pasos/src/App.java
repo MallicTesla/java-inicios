12b1df8af87dfdd1e51b238c09e1c60d2ac3ffa3
Mallic Tesla <el_mallic@hotmail.com>
2024-07-31 21:59:36 -0300

    continuando con el aprendizaje

--- primeros_pasos/src/App.java
+++ primeros_pasos/src/App.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Funcionoooooooooo");
 
-        String miString = "a las variavles se le tienen que declarar con el tipo ";
+        String miString = "a las variables se le tienen que declarar con el tipo ";
+        // final String miString = "cuando una variable comienza con final es una constante ";
 
-        miString = "una ves declarada la variable no es nesesario volverle a espesificar el tipo";
+        miString = "una ves declarada la variable no es necesario volverle a especificar el tipo";
         
         System.out.println(miString);
 
-        // se puede declarar un entero de dos formas utilisando solamente int o usando la clase Integer
+        // se puede declarar un entero de dos formas utilizando solamente int o usando la clase Integer
         int miEntero = 1 ;
-        // cuando se usa una clase tiene mas opsiones a la hora de trabajar con ese numero no solo las funciones vasicas
+        // cuando se usa una clase tiene mas opciones a la hora de trabajar con ese numero no solo las funciones básicas
         Integer miOtroEntero = 3 ;
 
         miOtroEntero = miOtroEntero + 3;
         System.out.println(miOtroEntero);
         System.out.println(miOtroEntero + 3);
 
-        // la clase Double tiene mas digitos desimales que Float
+        // la clase Double tiene mas dígitos decimales que Float
         Double conDesimal = 5.0;
         System.out.println(conDesimal);
 
@@ -25,12 +31,58 @@ public class App {
         Float numeroFlotante = 3.1f;
         System.out.println(numeroFlotante);
 
-        // no se ahi problemas de compativilidad entre los tipos de datos aunque se puede perder presision con los numeros Float y Double
+        // no se ahi problemas de compatibilidad entre los tipos de datos aunque se puede perder precisión con los números Float y Double
         System.out.println(conDesimal + " " + miEntero + " " + miString + numeroFlotante);
 
+        Boolean miBooliano = true;
+
+        // cuando se trabaja con una variable que puede ser null da error ahi que asegurarse de que eso no suceda
+        miOtroEntero = null ;
+        if (miOtroEntero != null) {
+            System.out.println (miOtroEntero + 5);
+        } else {
+            System.out.println ("miOtroEntero es null");
+        }
+
+        // para crear una lista primero se tiene que iniciar y después se le agrega la información
+        // para hacer una lista se necesita importar la clases
+        // lo que esta dentro de <> especifica el tipo de dato que puede almacenar esa lista (<Object> es para que almacenar todos los tipos)
+        List <Object> miLista = new ArrayList <> ();
+        miLista.add (miString);
+        miLista.add (miEntero);
+        System.out.println (miLista);
+
+        // así se crea un diccionario
+        Map <String, Object> miMap = new HashMap <> ();
+        miMap.put ("str", miString);
+        miMap.put ("int", miEntero);
+        System.out.println(miMap);
+        System.out.println(miMap.get ("int"));
+
+        for (int i = 0 ; i < miLista.size(); i++){
+            miLista.get (i);
+            System.out.println(miLista);
+            System.out.println(i);
+        }
 
 
+        int primerNumero = 2;
+        int segundoNumero = 3;
+        // así se llama una función donde (App) es la clase principal
+        App MiApp = new App();
+        // y luego se hacen instancias de esa función
+        MiApp.miFuncion (primerNumero, segundoNumero);
 
+        System.out.println("funcion2 " + MiApp.miFuncion2(primerNumero, segundoNumero));
+    }
+
+    // las funciones de tipo void no retornan nada
+    public void miFuncion (int primerNumero, int segundoNumero){
+        System.out.println(primerNumero + segundoNumero);
+    }
+
+    public int miFuncion2 (int primerNumero, int segundoNumero){
+        return primerNumero + segundoNumero ;
     }
 
 
